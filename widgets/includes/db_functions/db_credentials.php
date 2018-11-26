aa880308cc196aaea06c9792091d1f3eb38f7bdf
Thom Harrington <43864992+thomaskise@users.noreply.github.com>
2018-11-26 15:14:20 -0800

    remove credentials

--- widgets/includes/db_functions/db_credentials.php
+++
@@ -1,9 +0,0 @@
-<?php
-//credentials.php
-
-    define("DB_HOST", "school.kiseharrington.com");
-    define("DB_USER", "ed_db_admin");
-    define("DB_PASSWORD", "School1dude!!");
-    define("DB_NAME", "kise_class_db");
-
-?>
